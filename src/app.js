abb70aebd62e2c77a2499f2a26fb73549b1d7a3a
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2025-09-22 22:57:39 +0000

    made a fruit list

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const div = document.querySelector("#text");
+const fruits = ["apple", "banana", "cantelop", "dragonfruit", "fig"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const helloBtn = document.querySelector("#helloBtn");
+  const deleteBtn = document.querySelector("#deleteBtn");
+  const randomNumBtn = document.querySelector("#randomNumBtn");
+  const enterFruitBtn = document.querySelector("#enterFruitBtn");
+  helloBtn.addEventListener("click", helloWorld)
+  deleteBtn.addEventListener("click", deleteHelloWorld)
+  randomNumBtn.addEventListener("click", getRandomFruit)
+  enterFruitBtn.addEventListener("click", addToFruitArray)
 };
+
+const helloWorld = () => {
+  div.innerHTML = "Hello World";
+} 
+const deleteHelloWorld = () => {
+  div.innerHTML = "";
+} 
+
+const getRandomFruit = () => {
+  let randomNumber = Math.floor(Math.random() * fruits.length);
+  div.innerHTML = fruits[randomNumber];
+}
+
+const addToFruitArray = () => {
+  const input = document.querySelector("#fruitInput").value;
+  console.log(input, "INPUT")
+  fruits.push(input);
+  console.log(fruits);
+}
